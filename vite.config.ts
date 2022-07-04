a810e51945f1e7413155b8bb95c4f8b11aa0d6af
tooooo1 <wer4272@nate.com>
2022-07-05 05:44:40 +0900

    Chore: 절대 경로 및 eslint 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: 'assets',
+        replacement: resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: 'components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+      {
+        find: 'hooks',
+        replacement: resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: 'pages',
+        replacement: resolve(__dirname, 'src/pages'),
+      },
+    ],
+  },
 });
