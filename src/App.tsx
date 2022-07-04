0852d37fd1cd8681ea07099349fbd544363710d6
tooooo1 <wer4272@nate.com>
2022-07-05 06:35:23 +0900

    Feat: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Game from './pages/Game';
+import NotFound from './pages/NotFound';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>gd</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Game />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
